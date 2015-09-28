e52254049c9fdacbcb3da8b7450bf50119005f61
Hongyi Guo <hongyi.guo@utdallas.edu>
2015-09-28 14:14:17 -0500

    Initial Commit

---
+++ program/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  program
+//
+//  Created by Hongyi Guo on 9/28/15.
+//  Copyright © 2015 Hongyi Guo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
